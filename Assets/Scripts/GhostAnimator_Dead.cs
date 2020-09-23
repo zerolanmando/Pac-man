bcf5ed0021a6acf4683d5c21619e04fe729f3a96
zerolanmando <yiwei.shan@student.uts.edu.au>
2020-09-23 22:44:22 +0800

    create visual assets templates in scene

--- Assets/Scripts/GhostAnimator_Dead.cs
+++ Assets/Scripts/GhostAnimator_Dead.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class GhostAnimator_Dead : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public string sprite_name;
+	public int frame_rate = 12;
+	public SpriteRenderer sprite_renderer;
+	
+	private Sprite[] all_frames;
+	private int frame_index = 0;
+	
+	private float elapsed = 0.0f;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		all_frames = Resources.LoadAll<Sprite>(sprite_name);
+		
+		if(all_frames.Length > 0)
+		{
+			sprite_renderer.sprite = all_frames[frame_index];
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		elapsed += Time.deltaTime;
+	    
+		if(elapsed >= 1.0f / frame_rate)
+		{
+			elapsed = 0.0f;
+	    	
+			++frame_index;
+	    	
+			if(frame_index >= all_frames.Length)
+			{
+				frame_index = 0;
+			}
+	    	
+			sprite_renderer.sprite = all_frames[frame_index];
+		}
+	}
 }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+	public string sprite_name;
+	public int frame_rate = 12;
 	public SpriteRenderer sprite_renderer;
 	
+	private Sprite[] all_frames;
+	private int frame_index = 0;
+	
 	private float elapsed = 0.0f;
-	private int frame_rate = 12;
 	
     // Start is called before the first frame update
     void Start()
-    {
+	{
+		all_frames = Resources.LoadAll<Sprite>(sprite_name);
+		
+		if(all_frames.Length > 0)
+		{
+			sprite_renderer.sprite = all_frames[frame_index];
+		}
     }
 
     // Update is called once per frame
@@ -22,6 +32,15 @@ public class PlayerAnimator : MonoBehaviour
 	    if(elapsed >= 1.0f / frame_rate)
 	    {
 	    	elapsed = 0.0f;
+	    	
+	    	++frame_index;
+	    	
+	    	if(frame_index >= all_frames.Length)
+	    	{
+	    		frame_index = 0;
+	    	}
+	    	
+	    	sprite_renderer.sprite = all_frames[frame_index];
 	    }
     }
 }
